a78a484ab19ec597fd050a302f11a28034cb04c2
Muneeb Ur Rehman <munebkhan445@gmail.com>
2026-09-07 15:29:05 +0500

    Add Some field validator class method on email to make it lowercase always

--- Backend/app/schemas/user.py
+++ Backend/app/schemas/user.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field,field_validator
 
 
 class UserCreate(BaseModel):
     fullname: str = Field(min_length=1, max_length=255)
     email: EmailStr
     password: str = Field(min_length=8, max_length=128)
+    
+    @field_validator("email")
+    @classmethod
+    def normalize_email(cls, value: str) -> str:
+            return value.lower()
 
 
 class UserResponse(BaseModel):
@@ -24,3 +29,8 @@ class UserResponse(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+    
+    @field_validator("email")
+    @classmethod
+    def normalize_email(cls, value: str) -> str:
+        return value.lower()
