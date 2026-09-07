6c6e6b4947e6806357cb02579ed2ebdcb4229a72
Muneeb Ur Rehman <munebkhan445@gmail.com>
2026-09-07 16:54:56 +0500

    Add init for all directory from which we will do  imports

---
+++ Backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.base import Base
+from app.models.user import User
+from app.models.category import Category
+from app.models.task import Task
+
+__all__ = ["Base", "User", "Category", "Task"]
