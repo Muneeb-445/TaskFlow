7a3deb63abb0489812a3003eb27450804e9f0e6d
Muneeb Ur Rehman <munebkhan445@gmail.com>
2026-09-26 12:06:52 +0500

    Add our Frontend API Url

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -10,7 +10,10 @@ class Settings(BaseSettings):
     
     project_name: str = "TaskFlow"                    
     api_v1_prefix: str = "/api/v1"                     
-    backend_cors_origins: list[str] = [] 
+    backend_cors_origins: list[str] = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    ] 
          
     model_config = SettingsConfigDict(
         env_file=".env"
